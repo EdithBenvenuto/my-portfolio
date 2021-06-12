c9625ecb5834c5f1dd4cedcc371d21ba3ba3a0c2
Edith Benvenuto <edithpau_benvenuto@hotmail.com>
2021-06-12 02:45:22 +0000

    Add some style and a random fact button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,23 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+console.log(toggleButton);
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+})
+
+function addRandomFact() {
+  const greetings =
+      ['I love ketchup', 'I am from Mexico', "I love singing even i don't do it very well", "I don't like horror movies", "I am 20 years old", 'I love play videogames'];
+
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+  // Add it to the page.
+  const greetingContainer = document.getElementById('greeting-container-fact');
+  greetingContainer.innerText = greeting;
+}
