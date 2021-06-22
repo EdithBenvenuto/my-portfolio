07469e585b4982cffa95086d8149e0ef41c77044
Edith Benvenuto <edithpau_benvenuto@hotmail.com>
2021-06-22 06:16:37 +0000

    contact me server side

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,3 +17,37 @@ function addRandomFact() {
   const greetingContainer = document.getElementById('greeting-container-fact');
   greetingContainer.innerText = greeting;
 }
+
+function showMessages() {
+  fetch('/list-messages').then(response => response.json()).then((messages) => {
+    const messageListElement = document.getElementById('message-list');
+    messages.forEach((message) => {
+      messageListElement.appendChild(createMessageElement(message));
+    }) 
+  });
+}
+
+function createMessageElement(message) {
+    const messageElement = document.createElement('li');
+    messageElement.className = 'message';
+
+    const nameElement = document.createElement('p');
+    nameElement.innerText = 'Name: '+ message.name;
+
+    const emailElement = document.createElement('p');
+    emailElement.innerText = 'Email: ' + message.email;
+
+    const commentElement = document.createElement('p');
+    commentElement.innerText = 'Message: ' + message.comment;
+
+    var date = new Date(message.timestamp);
+    date.toString("MMM dd"); // "Dec 20"
+    const timestampElement = document.createElement('p');
+    timestampElement.innerText = 'Date: ' + date;
+
+    messageElement.appendChild(nameElement);
+    messageElement.appendChild(emailElement);
+    messageElement.appendChild(commentElement);
+    messageElement.appendChild(timestampElement);
+    return messageElement;
+}
